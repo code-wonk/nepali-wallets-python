0aea863cf1b266213d6d53d526a0a7021cc62549
Sudip Ghimire <ghimiresdp@gmail.com>
2022-04-03 23:50:58 +0545

    feat: added khalti client and esewa client (WIP)

---
+++ nepali_wallets/client/__init__.py
@@ -0,0 +1,2 @@
+from ._khalti import KhaltiClient
+from ._esewa import EsewaClient
---
+++ nepali_wallets/client/_khalti.py
@@ -0,0 +1,62 @@
+import json
+from typing import Union
+
+from .base import BasePaymentClient
+
+
+class KhaltiClient(BasePaymentClient):
+    transaction_pin: str
+    otp: str
+    website_url: str
+    return_url: str
+
+    def _get_request_headers(self) -> dict:
+        return {
+            "Authorization": f"Key {self.secret_key}",
+            'Content-Type': 'application/json',
+        }
+
+    def _get_request_body(self) -> dict:
+        return {
+            "public_key": self.public_key,
+        }
+
+
+    def create_intent(
+        self,
+        amount: int,
+        order_id: str,
+        order_name: str,
+        customer_info: dict,
+        amount_breakdown: Union[list, None] = None,
+        product_details: Union[list, None] = None
+
+    ):
+        return self.session.post(
+            f'{self.base_url}/epayment/initiate/',
+            data=json.dumps({
+                "return_url": self.return_url,
+                "website_url": self.website_url,
+                "amount": amount,  # amount in paisa
+                'purchase_order_id': order_id,
+                "purchase_order_name": order_name,
+                "customer_info": customer_info,
+                "amount_breakdown": amount_breakdown or [],
+                "product_details": product_details or []
+            }),
+            headers=self._get_request_headers()
+        )
+
+    def complete_payment(self, token: str, confirmation_code: str):
+        print("payment completion is automatically handled by the khalti client itself")
+        raise NotImplementedError()
+
+    def verify_payment(self, token: str):
+        raise NotImplementedError()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.sandbox:
+            self.base_url = 'https://a.khalti.com/api/v2'
+        else:
+            self.base_url = 'https://khalti.com/api/v2'
